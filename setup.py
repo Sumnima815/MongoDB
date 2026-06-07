c07600d75ab8caec1be48f4b728cf78ef768dfe7
Sumnima <sumnimaag@gmail.com>
2026-06-07 13:07:05 +0545

    project structure ready

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Sumnima Giri",
+    author_email="sumnimaag@gmail.com",
+    packages=find_packages()
+)
